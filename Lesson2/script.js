72797db64806b29276a5694bc928565c0869c258
Vitaliy Lazarenko <vila@ciklum.com>
2018-11-26 13:30:29 +0200

    Lesson 2

---
+++ Lesson2/script.js
@@ -0,0 +1,76 @@
+'use strict';
+
+let money = +prompt('Ваш бюджет на месяц?', ''),
+   time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+  budget: money,
+  timeData: time,
+  optionalExpenses: {},
+  income: [],
+  expenses: {},
+  savings: false,
+};
+
+for (let i = 0; i < 2; i++ ) {
+  let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+      b = prompt('Во сколько обойдется?', '');
+
+  if (typeof(a) === 'string' && (typeof(a) != null) && (typeof(b) != null)
+    && a != '' && b != '' && a.length < 50) {
+    console.log('done');
+    appData.expenses[a] = b;
+  } else {
+    i = 0;
+  }
+
+};
+
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//       b = prompt('Во сколько обойдется?', '');
+//
+//   if (typeof(a) === 'string' && (typeof(a) != null) && (typeof(b) != null)
+//     && a != '' && b != '' && a.length < 50) {
+//     console.log('done');
+//     appData.expenses[a] = b;
+//   }
+//
+//   i++;
+// }
+
+
+
+appData.moneyPerDay = appData.budget/30;
+
+alert('Your budget per 1 day - ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+  console.log('Min level')
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('Middle level')
+} else if (appData.moneyPerDay > 2000) {
+  console.log('Top level')
+} else {
+  console.log('Incorect data')
+}
+
+// switch (appData.moneyPerDay)  {
+//   case appData.moneyPerDay < 100:
+//     console.log('Min level')
+//     break;
+//   case (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000):
+//     console.log('Middle level')
+//     break;
+//   case appData.moneyPerDay > 2000:
+//     console.log('Top level')
+//     break;
+//   default:
+//     console.log('Incorect data')
+//     break;
+// }
+
+
+console.log(appData)
+
--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 'use strict';
 
-var money, time;
 
-var appData = {
-  budget: '',
-  timeData: new Date(),
-  optionalExpenses: '',
-  income: '',
+let money = +prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+  budget: money,
+  timeData: time,
+  optionalExpenses: {},
+  income: [],
   expenses: [],
   savings: false,
 };
 
-money = +prompt('Ваш бюджет на месяц?', '');
-time = prompt('Введите дату в формате YYYY-MM-DD', '');
+// if(money) { appData.budget = money }
+// if(time)  { appData.timeData = time }
 
-if(money) { appData.budget = money }
-if(time)  { appData.timeData = time }
-
-let question1, question2,
-    question3, question4;
-
-question1 = prompt('Введите обязательную статью расходов в этом месяце', '');
-question2 = prompt('Во сколько обойдется?', '');
-question3 = prompt('Введите обязательную статью расходов в этом месяце', '');
-question4 = prompt('Во сколько обойдется?', '');
+let question1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    question2 = prompt('Во сколько обойдется?', ''),
+    question3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    question4 = prompt('Во сколько обойдется?', '');
 
 appData.expenses[question1] = question2;
 appData.expenses[question3] = question4;
